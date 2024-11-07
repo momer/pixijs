7d62cc2a6362a64741cd8d7cac94fcf83542aded
Mo Omer <mo@ipv7.com>
2024-11-06 22:28:25 -0600

    lint floats

--- src/utils/math/floats.ts
+++ src/utils/math/floats.ts
@@ -1,8 +1,10 @@
 /**
- * Quickly rounds (with relatively decent precision) a number.
- * ref: https://stackoverflow.com/a/48764436
+ * Quickly rounds (with relatively decent precision) a number. ref: https://stackoverflow.com/a/48764436
+ * @param num
+ * @param decimalPlaces
  */
-export function quickRound(num: number, decimalPlaces: number): number {
+export function quickRound(num: number, decimalPlaces: number): number
+{
     const p = Math.pow(10, decimalPlaces || 0);
     const n = (num * p) * (1 + Number.EPSILON);
 
